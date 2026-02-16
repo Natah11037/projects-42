c80003deddd2bec3868bd22fb94e5aeb7a313e5a
Nathan Weber--Bouvier <nweber--@k1r3p7.42mulhouse.fr>
2026-02-16 10:32:21 +0100

    ex04 done

---
+++ piscine_python/P03/ex3/ft_achievement_tracker.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    print("=== Achievement Tracker System ===\n")
+    cihan_achievements = {
+        "Defeated_by_a_splinter",
+        "42_student",
+        "less_logtime",
+    }
+    kevin_achievements = {
+        "42_student",
+        "Tutor",
+        "ChatGPT_enjoyer",
+        "Stopped_smoking",
+    }
+    gautier_achievements = {"42_student", "code_slayer", "COD_Demon"}
+    print(f"Player Cihan achievements: {cihan_achievements}")
+    print(f"Player Kevin achievements: {kevin_achievements}")
+    print(f"Player Gautier achievements: {gautier_achievements}")
+    print("\n=== Achievement Analytics ===")
+    ouaboula = cihan_achievements.union(
+        kevin_achievements, gautier_achievements
+    )
+    print(f"All unique achievements: {ouaboula}")
+    print(f"Total unique achievements: {len(ouaboula)}\n")
+    common_all = cihan_achievements.intersection(
+        kevin_achievements, gautier_achievements
+    )
+    print(f"Common to all players: {common_all}")
+    oulilouli = cihan_achievements.difference(
+        kevin_achievements, gautier_achievements
+    )
+    ouliloulo = kevin_achievements.difference(
+        cihan_achievements, gautier_achievements
+    )
+    ouliloulu = gautier_achievements.difference(
+        cihan_achievements, kevin_achievements
+    )
+    rare_achievements = oulilouli.union(ouliloulo, ouliloulu)
+    print(f"Rare achievement (1 Player): {rare_achievements}\n")
+    cihan_vs_kevin = cihan_achievements.intersection(kevin_achievements)
+    print(f"Cihan vs Kevin common: {cihan_vs_kevin}")
+    cihan_unique = cihan_achievements.difference(
+        kevin_achievements, gautier_achievements
+    )
+    print(f"Cihan unique: {cihan_unique}")
+    kevin_unique = kevin_achievements.difference(
+        cihan_achievements, gautier_achievements
+    )
+    print(f"Kevin unique: {kevin_unique}")
