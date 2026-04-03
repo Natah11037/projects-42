18a69022d3d44a223e6cb7be9aef59b765311514
root <root@PCNatah.localdomain>
2026-04-03 12:01:31 +0200

    yeah

--- piscine_python/P08/ex1/loading.py
+++ piscine_python/P08/ex1/loading.py
@@ -1,39 +1,22 @@
 import importlib
-from importlib.metadata import PackageNotFoundError, metadata
-import re
-import sys
-import tomllib
 
 
 def check_depedencies():
-    try:
-        with open("pyproject.toml", "rb") as f:
-            data = tomllib.load(f)
-        dependencies = data["project"].get("dependencies", [])
-    except FileNotFoundError:
-        return "Error: file pyproject.toml was not found"
-
-    for d in dependencies:
-        package_name: str = re.split(r"[><=!;\s\[]", d)[0].strip()
+    dep_list = ["numpy", "pandas", "matplotlib", "requests"]
+    for dep in dep_list:
         try:
-            meta = metadata(package_name)
-            print(f"\r    [OK] {meta['Name']} {meta['Version']} - found")
-        except PackageNotFoundError:
-            print(f"\r     [KO] package {package_name} "
-                  "was not found\n\n"
-                  "=======================================\n"
-                  "Install it with:\n"
-                  "pip install -r requirements.txt\n"
+            module = importlib.import_module(dep)
+            print(f"[OK] {dep} ({module.__version__}) - is installed.")
+        except ImportError:
+            print(f"[KO] {dep} is not installed.\n"
+                  "To install it, run the following command:\n"
+                  f"pip install {dep}\n"
                   "or\n"
-                  "poetry install\n"
-                  "=======================================\n")
-            continue
+                  f"poetry add {dep}\n")
 
 
 def main():
-    print()
     check_depedencies()
-    print()
 
 
 if __name__ == "__main__":
