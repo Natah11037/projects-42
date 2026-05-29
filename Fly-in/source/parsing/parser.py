a15cf8b04256429766ca9ad6fe6006506a6c4b43
Nathan Weber--Bouvier <nweber--@k1r3p5.42mulhouse.fr>
2026-05-29 11:33:00 +0200

    continue

--- Fly-in/source/parsing/parser.py
+++ Fly-in/source/parsing/parser.py
@@ -2,12 +2,23 @@ class Parser():
     def __init__(self, map: str):
         self.map = map
 
-    def parse(self):
-        valid_lignes = self._read_file()
-        if valid_lignes is None:
+    def parse(self) -> dict:
+        data = {
+            "start hub": None,
+            "hub": [],
+            "end hub": None,
+            "connections": [],
+            "nb_drones": None
+        }
+        valid_lines = self._read_file()
+        if valid_lines is None:
             return None
-        for index, ligne in valid_lignes:
-            self._parse_ligne(ligne)
+        for i, (index, line) in enumerate(valid_lines):
+            try:
+                self._parse_ligne(line, i, index)
+            except ValueError as e:
+                print(e)
+                return None
 
     def _read_file(self):
         try:
@@ -17,12 +28,27 @@ class Parser():
             print(f"Error: File '{self.map}' not found or could not be read.")
             return None
         valid_lignes = []
-        for index, line in enumerate(lines):
+        for index, line in enumerate(lines, start=1):
             line = line.strip()
             if line.startswith('#') or not line:
                 continue
             else:
                 valid_lignes.append((index, line))
         return valid_lignes
-    
-    def _parse_ligne(self, ligne: str):
+
+    def _parse_ligne(self, line: str, i: int, index: int):
+        if i == 0:
+            if line.startswith("nb_drones:"):
+                try:
+                    nb_drones = int(line.split(":")[1].strip())
+                    if nb_drones <= 0:
+                        raise ValueError("Error: Number of drones cannot "
+                                         "be negative or zero on "
+                                         f"line {index}.")
+                    else:
+                        self.data["nb_drones"] = nb_drones
+                except ValueError:
+                    print(f"Error: Invalid number of drones on line {index}.")
+            else:
+                raise ValueError("Error: First line must specify "
+                                 f"number of drones on line {index}.")
